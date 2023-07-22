3b7cf261dbbeb8e3709b1d0d34da07111728eaa5
Suliman <sssuliman@bk.ru>
2023-07-22 14:14:10 +0300

    Сделал компонент Header

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,10 +1,11 @@
 import { FC } from "react";
 import styles from "./main.module.scss";
+import Header from "../../components/Header/Header";
 
 const Main: FC = () => {
-
   return (
     <div className={styles.main}>
+      <Header />
       <h1>It`s Main page</h1>
     </div>
   );
