600e2587aacd7fdca0362e333f526cef049628b6
Djamalayl <djamalayl32@mail.ru>
2023-07-26 22:00:33 +0300

    add

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import { Provider } from "react-redux/es/exports";
 import {store} from "./app/store.ts";
 import './normalize.css'
 import "./index.module.scss";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
