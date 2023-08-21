55d11720e05e84dcc66479f839c222ee599a3da5
GooA-NA <nasirgudesev@gmail.com>
2023-08-21 13:58:15 +0300

    fix: Изменена картика для страницы error404, добавлена навигация на главную страницу при нажатии кнопки

--- src/components/Error404/Error404.tsx
+++ src/components/Error404/Error404.tsx
@@ -1,13 +1,18 @@
 import { FC } from "react";
 import styles from "./error404.module.scss";
 import error404 from '../../assets/icons/Error404.png'
+import { useNavigate } from "react-router-dom";
 
 const Error404: FC = () => {
+  const navigate = useNavigate();
+  const handleClick = () :void => {
+    navigate('/')
+  }
   return (
       <div className={styles.error}>
         <img src={error404} alt="ошибка 404" />
         <span>Упс! Страница не найдена</span>
-        <button>Вернуться на главную</button>
+        <button onClick={handleClick}>Вернуться на главную</button>
       </div>
   );
 };
