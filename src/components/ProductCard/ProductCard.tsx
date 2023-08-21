e6051ada3d3e14aee87cd324ba3511d58bd0fa9d
Suliman <sssuliman@bk.ru>
2023-08-21 13:38:23 +0300

    fix: Сделал обработчик на loading и error

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import styles from "./productCard.module.scss";
 import { useAppDispatch, useAppSelector } from "../../app/hook";
 import {
@@ -6,15 +6,17 @@ import {
   addToFavorite,
   deleteToFavorite,
 } from "../../features/favoriteSlice";
+import { addToBasket } from "../../features/basketSlice";
 import HeartSVG from "../../assets/icons/HeartSVG";
-import { useNavigate } from "react-router-dom";
+import CustomAlert from "../Alert/CustomAlert";
+import { alertState } from "../Alert/alertState";
 
 interface ProductCardProps {
   _id: string;
   name: string;
   title: string;
   price: number;
-  photo: string;
+  photo: [string];
 }
 
 const ProductCard: FC<ProductCardProps> = ({
@@ -24,7 +26,6 @@ const ProductCard: FC<ProductCardProps> = ({
   price,
   photo,
 }) => {
-  const navigate = useNavigate();
   const dispatch = useAppDispatch();
   const userId = useAppSelector((state) => state.applicationSlice.userId);
   const user = useAppSelector((state) => state.userSlice.user);
@@ -34,7 +35,10 @@ const ProductCard: FC<ProductCardProps> = ({
     )
   );
 
-  // Нужно перенести эту логику на страницу с полным описанием товара
+  const [openAlert, setOpenAlert] = useState(false);
+  const [message, setMessage] = useState("");
+  const [alert, setAlert] = useState<alertState>(alertState.success);
+
   // Заменить везда цифру 40 на нужный размер
   const handleAddFavorite = (e: React.MouseEvent<HTMLDivElement>) => {
     e.stopPropagation();
@@ -47,6 +51,9 @@ const ProductCard: FC<ProductCardProps> = ({
             size: 40,
           })
         );
+        setMessage(() => "Товар добавлен в избранное");
+        setOpenAlert(true);
+        setAlert(alertState.success);
       } else {
         dispatch(
           deleteToFavorite({
@@ -55,31 +62,53 @@ const ProductCard: FC<ProductCardProps> = ({
             size: 40,
           })
         );
+        setMessage(() => "Товар удален из избранных");
+        setOpenAlert(true);
+        setAlert(alertState.success);
       }
+    } else {
+      setMessage(() => "Вы не вошли в аккаунт");
+      setOpenAlert(true);
+      setAlert(alertState.error);
+    }
+  };
+
+  const handleAddBasket = () => {
+    if (userId) {
+      dispatch(addToBasket({ id: user.basket, productId: _id, size: 40 }));
     }
   };
 
   return (
-    <div
-      className={styles.product}
-      onClick={() => navigate(`/productDetail/${_id}`)}
-    >
-      <div className={styles.productImg}>
-        <img src={`http://localhost:3010/${photo[0]}`} alt="Футболка" />
-      </div>
-      <div className={styles.favorite} onClick={handleAddFavorite}>
-        <HeartSVG className={favorites ? styles.active : styles.none} />
-      </div>
-      <div className={styles.productInfo}>
-        <div className={styles.description}>
-          <div className={styles.title}>
-            <span>{name}</span>
-            <span>{title}</span>
+    <>
+      {openAlert && (
+        <CustomAlert
+          setOpenAlert={setOpenAlert}
+          alert={alert}
+          message={message}
+        />
+      )}
+      <div className={styles.product}>
+        <div className={styles.productImg}>
+          <img src={`http://localhost:3010/${photo[0]}`} alt="Футболка" />
+        </div>
+        <div className={styles.favorite} onClick={handleAddFavorite}>
+          <HeartSVG className={favorites ? styles.active : styles.none} />
+        </div>
+        <div className={styles.productInfo}>
+          <div className={styles.description}>
+            <div className={styles.title}>
+              <span>{name}</span>
+              <span>{title}</span>
+            </div>
+            <button>${price}</button>
           </div>
-          <button>${price}</button>
+          <button onClick={handleAddBasket} className={styles.buy}>
+            В корзину
+          </button>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
