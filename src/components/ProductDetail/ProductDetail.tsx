2eae91ae40c0b8878faa9c8f979ff87d39ebf853
Djamalayl <djamalayl32@mail.ru>
2023-09-21 21:44:43 +0300

    fixed busket link

--- src/components/ProductDetail/ProductDetail.tsx
+++ src/components/ProductDetail/ProductDetail.tsx
@@ -26,32 +26,7 @@ const ProductDetail: React.FC = () => {
         
     }, [dispatch])
     
- 
-
-    // const slides: string[] = [
-    //     "https://fikiwiki.com/uploads/posts/2022-02/1644972802_1-fikiwiki-com-p-kartinki-krasivie-na-android-1.jpg",
-    //     "https://img1.akspic.ru/previews/7/4/7/9/6/169747/169747-ikanvas-art-pechat_na_holste-poster-oblako-500x.jpg",
-    //     "https://img3.akspic.ru/previews/6/4/2/8/6/168246/168246-skazhi_igru-lyudo_king-kosti-igra_v_kosti-azartnaya_igra-500x.jpg",
-    // ];
-    // const [currentSlide, setCurrentSlide] = useState<number>(0);
-
-    
-    // const showSlide = (index: number): void => {
-    //     setCurrentSlide(index);
-    // };
-
-    // const nextSlide = (): void => {
-    //     setCurrentSlide((currentSlide + 1) % slides.length);
-    // };
-
-    // const prevSlide = (): void => {
-    //     setCurrentSlide((currentSlide - 1 + slides.length) % slides.length);
-    // };
-
-    // const handleScroll = (index: React.MouseEvent<HTMLDivElement>): void => {
-    //     setCurrentSlide(+index);
-    // };
-
+  
     const user = useAppSelector((state) => state.userSlice.user.basket);
 const sizes = useAppSelector((state) => state.productSlice.products)
 
@@ -66,8 +41,6 @@ const sizes = useAppSelector((state) => state.productSlice.products)
             
         }));
     };
-
-  
    
     return (
         <>
